02d5afb7be9783894a7805a66f2c4299420dfe67
SaiKrishnaMedishetty <saiabhi096@gmail.com>
2025-08-26 16:58:26 -0700

    jenkinsfile

---
+++ receipt.py
@@ -0,0 +1,8 @@
+print("Helloo, welcome to Pizza Hut")
+print("How can i help you")
+
+customer_name = input("May I know your name: ")
+item_name = input("what do you want: ")
+
+for item_name in pizza:
+    
