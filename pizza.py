72bea44f864894e2b8d845e0ac643859d3fdfe02
SaiKrishnaMedishetty <saiabhi096@gmail.com>
2025-08-26 22:49:51 -0700

    complete

--- pizza.py
+++ pizza.py
@@ -1,1 +1,6 @@
 pizza = [ "veg", "non-veg", "vegan","egg"]
+item_price = {
+    "veg": 10,
+    "non-veg":14,
+    "vegan": 16
+    }
--- receipt.py
+++ receipt.py
@@ -1,14 +1,30 @@
-import pizza
+from pizza import *
 
 print("Helloo, welcome to Pizza Hut")
 print("How can i help you")
-
-customer_name = input("May I know your name: ")
-item_name = input("what do you want: ")
-
 cart = []
 
-for item_name in pizza:
-    cart.append[item_name]
+customer_name = input("May I know your name: ")
+product = input("what do you want: ")
+if product in pizza:
+    cart.append(product)
+
+else: 
+    print("we dont have this product")
+quantity = int(input("please enter the quantity: "))
+
+
+
+
+
+
+if product == "veg" or "non-veg" or "vegan" in pizza:
+    Total_amount = quantity * item_price[product]
+    
+print(f"the total amount is: {Total_amount}")
+
+
+
+
 
 
